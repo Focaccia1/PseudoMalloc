675bae2ee58b3133f68fbf8d74fef5dd111eea47
Giovanni Zara <129419432+Focaccia1@users.noreply.github.com>
2023-10-16 17:19:46 +0200

    added comment and minor changes

--- bit_map.c
+++ bit_map.c
@@ -1,36 +1,37 @@
 #include <assert.h>
 #include "bit_map.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-// returns the number of bytes to store bits booleans
-int BitMap_getBytes(int bits){
-  return bits/8 + (bits%8)!=0;
+int BitMap_getBytes(int bits){  //calcolo i byte necessari per memorizzare un numero x di bit(preso in input)
+  return bits/8 + (bits%8)!=0;  //bits/8 mi da il numero di byte e poi controllo se serve aggiungere un altro byte extra epr bit avanzati
 }
 
-// initializes a bitmap on an external array
-void BitMap_init(BitMap* bit_map, int num_bits, uint8_t* buffer){
+void BitMap_init(BitMap* bit_map, int num_bits, uint8_t* buffer){       //inizializzo la bitmap con un array
   bit_map->buffer=buffer;
   bit_map->num_bits=num_bits;
   bit_map->buffer_size=BitMap_getBytes(num_bits);
 }
 
-// sets a the bit bit_num in the bitmap
-// status= 0 or 1
-void BitMap_setBit(BitMap* bit_map, int bit_num, int status){
-  // get byte
-  int byte_num=bit_num>>3;
+
+void BitMap_setBit(BitMap* bit_map, int bit_num, int status){   //setto un bit specifico nella bitmap, calcolo l byte che contiene il bit in questione
+                                                                //e poi lo setto in base allo status in unput
+
+  int byte_num=bit_num>>3;  //come se dividessi per 2^3, cioè 8, per ottenere il numero di byte
   assert(byte_num<bit_map->buffer_size);
-  int bit_in_byte=byte_num&0x03;
-  if (status) {
+  int bit_in_byte=byte_num&0x03;    //AND bit a bit per ottenere il bit in questione
+  if (status) {                    //se lo status è 1 allora setto il bit
     bit_map->buffer[byte_num] |= (1<<bit_in_byte);
-  } else {
+  } else {                       //altrimenti lo resetto (eseguo not bitwise)
     bit_map->buffer[byte_num] &= ~(1<<bit_in_byte);
   }
 }
 
 // inspects the status of the bit bit_num
 int BitMap_bit(const BitMap* bit_map, int bit_num){
-  int byte_num=bit_num>>3; 
+  int byte_num=bit_num>>3; //trovo numero del byte
   assert(byte_num<bit_map->buffer_size);
-  int bit_in_byte=byte_num&0x03;
-  return (bit_map->buffer[byte_num] & (1<<bit_in_byte))!=0;
+  int bit_in_byte=byte_num&0x03;    //AND bit a bit per ottenere il bit in questione
+  return (bit_map->buffer[byte_num] & (1<<bit_in_byte))!=0; //ritorno il bit in questione(1 acceso, 0 spento)
 }
