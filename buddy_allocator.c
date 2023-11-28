be5a559c061ddeee5f9e8089a4968ee613a07380
Giovanni Zara <129419432+Focaccia1@users.noreply.github.com>
2023-11-28 11:18:41 +0100

    minor fixes and new functions to buddy_allocator.c

--- buddy_allocator.c
+++ buddy_allocator.c
@@ -18,7 +18,7 @@ int get_parent(int index){
         return floor((index-1)/2); //calcolo il padre
 }
 
-int get_buddy(index){
+int get_buddy(index){  //per trovare index del buddy di un blocco dato(index input)
     if (index == 0)
         return -1; //root non ha buddy
     else if (index % 2 == 0) //se è pari -> il buddy è index -1
@@ -39,9 +39,15 @@ get_first_buddy(int level){
     return (int) (1 << level) - 1; //calcolo primo buddy (bitwise -> 2^index)
 }
 
-void check_index(buddy_allocator_t * buddy_allocator, int index){
-  if(index < 0 || index > (1 << buddy_allocator->lvl) - 1){
+void check_index(buddy_allocator_t * buddy_allocator, int index){  //controlla se index è nei limiti del buddy [0, 2^(buddy->lvl)]
+  if(index < 0 || index > (1 << buddy_allocator->lvl) - 1){ 
     perror("error: invalid index");
   }
 }
 
+void buddy_allocator_init(buddy_allocator_t *buddy_allocator, void *buffer, BitMap *bitmap, int lvl, int min){    //inizializza allocator
+  buddy_allocator->min_block_size = min;  //dim minima del blocco
+  buddy_allocator->memory = buffer;  //ptr memoria allocata
+  buddy_allocator->bitmap = bitmap;  //ptr bitmap (vd. bitmap.h)
+  buddy_allocator->lvl = lvl;
+}
