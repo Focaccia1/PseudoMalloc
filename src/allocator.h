e78c76cdbe661f85ae49512798da38d1222ece5b
Focaccia1 <zara.1929181@studenti.uniroma1.it>
2024-04-29 20:42:52 +0200

    made comments nicier and tests.c setup

--- src/allocator.h
+++ src/allocator.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <stdint.h>
 
-#define MAX_MEM_SIZE (1 << 20)   //1024*1024 as the example showed in course
+#define MAX_MEM_SIZE (1 << 20)   //1024*1024 as the example showed in classes
 #define BUDDY_LVLS 9    //10 cause there is 0
 #define MIN_SIZE (MAX_MEM_SIZE >>(BUDDY_LVLS - 1))     //It's like dividing the max size by 2^9, that is the number of levels. So >I obtain the minimum size
 #define N_BUDDIES ((1 << BUDDY_LVLS) - 1)   //2^9 - 1 = 511
